58eadefcb30617bd784ff4d8715ee365d1b48865
Tanya <nickolaychuk.t@gmail.com>
2017-02-17 13:09:00 +0200

    head

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp"),
+	browserSync = require('browser-sync');
+
+// Static server
+gulp.task('server', function () {
+    browserSync({
+    	port:9000,
+        server: {
+            baseDir: 'app'
+        }
+    });
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch([
+    	'app/*.html',
+    	'app/js/**/*.js',
+		'app/css/**/*.css'
+	]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+КАК установить мой проект
+
+1. git clone https://github.com/t-nick/bakery
+2. cd site
+3. bower i
+4. npm i
+5. gulp
